a860d86067bd04dc2a75d8180082083f352f0f4f
Eduardo Margulis <edumarg@gmx.com>
2021-03-30 16:23:45 +0300

    improved current coin format to display

--- main.py
+++ main.py
@@ -29,12 +29,12 @@ if __name__ == "__main__":
             {
                 "symbol": "BTC",
                 "amount_owned": 2000,
-                "price_payed_per_unit": 20
+                "price_payed_per_unit": 20.0
             },
             {
                 "symbol": "ETH",
                 "amount_owned": 500,
-                "price_payed_per_unit": 2
+                "price_payed_per_unit": 2.0
             },
             {
                 "symbol": "XPR",
@@ -49,7 +49,7 @@ if __name__ == "__main__":
             {
                 "symbol": "EOS",
                 "amount_owned": 1000,
-                "price_payed_per_unit": 2
+                "price_payed_per_unit": 2.0
             },
         ]
         portfolio_profit_loos = 0
@@ -61,21 +61,24 @@ if __name__ == "__main__":
                     total_current_value = sym["amount_owned"] * float(coin["quote"]["USD"]["price"])
                     profit = total_current_value - total_paid
                     profit_percentage = profit / total_paid * 100
-                    print("---------------")
+                    profit_per_coin = float(coin["quote"]["USD"]["price"]) - sym["price_payed_per_unit"]
+                    print("---------------------------------------------")
                     my_coin = {"Name": coin["name"],
                                "Symbol": coin["symbol"],
                                "Rank": coin["cmc_rank"],
-                               "Price": "${0:.2f}".format(float(coin["quote"]["USD"]["price"])),
+                               "Current Price": "${0:.2f}".format(float(coin["quote"]["USD"]["price"])),
                                "24 Hour Change:": "{0:.2f}%".format(float(coin["quote"]["USD"]["percent_change_24h"])),
+                               "Paid per coin": "${0:.2f}".format(float(sym["price_payed_per_unit"])),
                                "Amount Owned": f'{sym["amount_owned"]} units',
-                               "Total Paid": "${0:.2f}".format(total_paid),
                                "Total current value": "${0:.2f}".format(total_current_value),
+                               "Total Paid": "${0:.2f}".format(total_paid),
+                               "Profit/Loss per coin:": "${0:.2f}".format(profit_per_coin),
                                "Profit/Loss:": "${0:.2f}".format(profit),
                                "Profit/Loss percentage": "{0:.2f}%".format(profit_percentage),
                                }
                     portfolio_profit_loos += profit
                     pprint(my_coin)
-        print("---------------")
+        print("---------------------------------------------")
         print("Portfolio Total Profit/Loos: ", "${0:.2f}".format(portfolio_profit_loos))
 
     except (ConnectionError, Timeout, TooManyRedirects) as e:
